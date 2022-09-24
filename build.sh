5801c37575347bbcec4c37653058c069871aa19a
Georges Savoundararadj <savoundg@gmail.com>
2022-09-24 16:35:45 -0700

    build.sh: add script

---
+++ build.sh
@@ -0,0 +1,23 @@
+REPO="caddy"
+CADDY_VERSION="v2.2.0"
+IMAGE="caddy"
+DOCKERFILE_HASH=$(git rev-parse --short HEAD)
+BUILDER="golang-1.15.3-alpine3.12"
+
+docker_build_tag_and_push()
+{
+	BUILD_ARG="$2"
+	TAG="${IMAGE}:${BUILDER}-${CADDY_VERSION}"
+
+	docker build "https://github.com:/manoj23/dockerfile-${REPO}.git" \
+		--build-arg "CADDY_VERSION=${CADDY_VERSION}" \
+		--build-arg "BUILD_ARGS=${BUILD_ARG}" \
+		--build-arg "DOCKERFILE_HASH=${DOCKERFILE_HASH}" \
+		-t "$TAG"
+
+	docker tag "${TAG}" "ghcr.io/manoj23/${TAG}"
+	docker push "ghcr.io/manoj23/${TAG}"
+}
+
+docker_build_tag_and_push "caddy" ""
+docker_build_tag_and_push "caddy-webdav" "--with github.com/mholt/caddy-webdav"
